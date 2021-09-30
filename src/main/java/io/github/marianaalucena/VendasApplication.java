d9b806199939a213e41d9f83cca713c0a4fb607a
Mariana Araujo <mariana.a.lucena@accenture.com>
2021-09-29 22:04:11 -0300

    Alterando de jdbc para jpa

--- src/main/java/io/github/marianaalucena/VendasApplication.java
+++ src/main/java/io/github/marianaalucena/VendasApplication.java
@@ -18,31 +18,60 @@ public class VendasApplication {
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
             System.out.println("SALVANDO CLIENTES");
-            clientes.salvar(new Cliente("Mariana"));
-            clientes.salvar(new Cliente("Pedro"));
+            clientes.save(new Cliente ("Mariana"));
+            clientes.save(new Cliente ("Pedro"));
 
-            List<Cliente> todos = clientes.obterTodos();
+            List<Cliente> todos = clientes.findAll();
             todos.forEach(System.out::println);
 
             System.out.println("ATUALIZANDO CLIENTES");
             todos.forEach(c -> {
               c.setNome(c.getNome() + " atualizado");
-              clientes.atualizar(c);
+              clientes.save(c);
             });
 
-            System.out.println("BUSCANDO CLIENTES POR NOME");
-            clientes.buscarPorNome("ana").forEach(System.out::println);
+            todos = clientes.findAll();
+            todos.forEach(System.out::println);
 
-//            System.out.println("EXCLUINDO CLIENTES");
-//            clientes.obterTodos().forEach(c-> {
-//                clientes.deletar(c);
-//            });
-            todos = clientes.obterTodos();
+            System.out.println("BUSCANDO CLIENTES POR NOME");
+            clientes.findByNomeLike("ana").forEach(System.out::println);
+
+            System.out.println("EXCLUINDO CLIENTES");
+            clientes.findAll().forEach(c-> {
+                clientes.delete(c);
+            });
+            todos = clientes.findAll();
             if(todos.isEmpty()){
                 System.out.println("Nenhum cliente encontrado");
             } else {
                 todos.forEach(System.out::println);
             }
+
+//            clientes.salvar(new Cliente("Mariana"));
+//            clientes.salvar(new Cliente("Pedro"));
+
+//            List<Cliente> todos = clientes.obterTodos();
+//            todos.forEach(System.out::println);
+//
+//            System.out.println("ATUALIZANDO CLIENTES");
+//            todos.forEach(c -> {
+//              c.setNome(c.getNome() + " atualizado");
+//              clientes.atualizar(c);
+//            });
+
+//            System.out.println("BUSCANDO CLIENTES POR NOME");
+//            clientes.buscarPorNome("ana").forEach(System.out::println);
+
+//            System.out.println("EXCLUINDO CLIENTES");
+//            clientes.obterTodos().forEach(c-> {
+//                clientes.deletar(c);
+//            });
+//            todos = clientes.obterTodos();
+//            if(todos.isEmpty()){
+//                System.out.println("Nenhum cliente encontrado");
+//            } else {
+//                todos.forEach(System.out::println);
+//            }
 
         };
 
---
+++ src/main/java/io/github/marianaalucena/domain/repositorio/ClientesJpa.java
@@ -0,0 +1,57 @@
+package io.github.marianaalucena.domain.repositorio;
+
+import io.github.marianaalucena.domain.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class ClientesJpa {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
+    public Cliente salvar(Cliente cliente){
+        entityManager.persist(cliente);
+        return  cliente;
+    }
+
+    @Transactional
+    public Cliente atualizar(Cliente cliente){
+        entityManager.merge(cliente);
+        return cliente;
+    }
+
+    @Transactional
+    public void deletar(Cliente cliente){
+        if(!entityManager.contains(cliente)){
+            cliente = entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente);
+    }
+
+    @Transactional
+    public void deletar(Integer id){
+        Cliente cliente = entityManager.find(Cliente.class, id);
+        entityManager.remove(cliente);
+    }
+
+//    @Transactional(readOnly = true)
+//    public List<Cliente> buscarPorNome(String nome){
+//        String jpql = "select c from Cliente c where c.nome like :nome";
+//        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+//        query.setParameter(name: "nome", value: "%" + nome + "%");
+//        return query.getResultList();
+//    }
+//
+//    @Transactional
+//    public List<Cliente> obterTodos(){
+//        return entityManager.createQuery( qlString: "from Cliente", Cliente.class)
+//            .getResultList();
+//    }
+
+
+}
